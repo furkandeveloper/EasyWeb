faed709f53d9e36fd6fd3c9168cf9008656ea224
furkandeveloper <furkan.dvlp@gmail.com>
2022-04-19 01:35:35 +0300

    🔨 Implements EasyWeb.AspNetCore.Filters

--- sample/EasyWeb.Demo/Program.cs
+++ sample/EasyWeb.Demo/Program.cs
@@ -1,11 +1,12 @@
 using System.Text.Json.Serialization;
 using EasyWeb.AspNetCore.ApiStandarts;
+using EasyWeb.AspNetCore.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews()
-    .AddJsonOptions(options => { options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()); });
-
+    .AddJsonOptions(options => { options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()); })
+    .AddEasyWebCore();
 
 builder.Services.ConfigureWebApiStandarts();
 
