38de0b8da83d2991ae86bc117259eefabb982408
furkandeveloper <furkan.dvlp@gmail.com>
2022-04-19 00:04:35 +0300

    🔨 Implement AuthorizationOperationFilter.cs

---
+++ src/EasyWeb.AspNetCore.Swagger/Filters/AuthorizationOperationFilter.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace EasyWeb.AspNetCore.Swagger.Filters;
+
+/// <summary>
+/// Authorization Operation Filter for Swagger
+/// </summary>
+public class AuthorizationOperationFilter : IOperationFilter
+{
+    /// <summary>
+    /// Apply Operation Filter for Authorization
+    /// </summary>
+    /// <param name="operation">
+    /// Operation Object see <see cref="OpenApiOperation"/>
+    /// </param>
+    /// <param name="context">
+    /// Operation Filter Context. See <see cref="OperationFilterContext"/>
+    /// </param>
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+        if (context.ApiDescription.ActionDescriptor.EndpointMetadata.FirstOrDefault(x => x is AuthorizeAttribute) is
+            AuthorizeAttribute filter)
+        {
+            operation.Security = (filter.AuthenticationSchemes ?? filter.Policy)?.Split(',')
+                .Select(s => new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = s,
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                })
+                .ToList();
+        }
+    }
+}
