3ba7b5bcf0d1b21e293e275ba9fa628c1ac49d2d
furkandeveloper <furkan.dvlp@gmail.com>
2022-04-19 01:33:54 +0300

    🔨 Implement EasyWeb.AspNetCore.Standard library

--- sample/EasyWeb.Demo/Program.cs
+++ sample/EasyWeb.Demo/Program.cs
@@ -1,4 +1,14 @@
+using System.Text.Json.Serialization;
+using EasyWeb.AspNetCore.ApiStandarts;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews()
+    .AddJsonOptions(options => { options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()); });
+
+
+builder.Services.ConfigureWebApiStandarts();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
