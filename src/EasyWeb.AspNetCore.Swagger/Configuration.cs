d94ef22f8aa12d660c83d38a1431963269bafba4
furkandeveloper <furkan.dvlp@gmail.com>
2022-04-21 00:04:33 +0300

    🔨 Add Configuration file for swagger

---
+++ src/EasyWeb.AspNetCore.Swagger/Configuration.cs
@@ -0,0 +1,17 @@
+namespace EasyWeb.AspNetCore.Swagger;
+
+/// <summary>
+/// Swagger Configuration Object
+/// </summary>
+public class Configuration
+{
+    /// <summary>
+    /// Document Title
+    /// </summary>
+    public static string Title => null;
+
+    /// <summary>
+    /// Document Description
+    /// </summary>
+    public static string Description => null;
+}
