780e528a0328cde5156c257201bffabb5af0004f
furkandeveloper <furkan.dvlp@gmail.com>
2022-04-22 00:12:57 +0300

    🔨 Implement Markdown documenting

--- sample/EasyWeb.Demo/Program.cs
+++ sample/EasyWeb.Demo/Program.cs
@@ -2,6 +2,7 @@ using System.Text.Json.Serialization;
 using EasyWeb.AspNetCore.ApiStandarts;
 using EasyWeb.AspNetCore.Filters;
 using EasyWeb.AspNetCore.Swagger;
+using MarkdownDocumenting.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,9 +22,22 @@ builder.Services.ConfigureSwagger(options =>
     options.Description = "Demo Service Description";
 });
 
+builder.Services.AddDocumentation();
+
 var app = builder.Build();
 
 app.UseRouting();
+
+app.UseDocumentation(config =>
+{
+    config.HighlightJsStyle = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.13.1/styles/github.min.css";
+    config.GetMdlStyle = "https://code.getmdl.io/1.1.3/material.teal-orange.min.css";
+    config.RootPathHandling = HandlingType.Redirect;
+    config
+        .AddCustomLink(new MarkdownDocumenting.Elements.CustomLink("Swagger", "/api-docs"))
+        .AddFooterLink(new MarkdownDocumenting.Elements.CustomLink("furkandeveloper", "https://github.com/furkandeveloper"))
+        .AddFooterLink(new MarkdownDocumenting.Elements.CustomLink("Repository", "https://github.com/furkandeveloper/EasyWeb"));
+});
 
 app.ApplySwaggerConfiguration();
 
