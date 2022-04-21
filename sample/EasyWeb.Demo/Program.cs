c6814eb2da3287b39b14fe70fcf9fd12e8eb9a2e
furkandeveloper <furkan.dvlp@gmail.com>
2022-04-21 23:59:59 +0300

    🔨 Add json value ignore configuration for Program.cs

--- sample/EasyWeb.Demo/Program.cs
+++ sample/EasyWeb.Demo/Program.cs
@@ -1,17 +1,38 @@
 using System.Text.Json.Serialization;
 using EasyWeb.AspNetCore.ApiStandarts;
 using EasyWeb.AspNetCore.Filters;
+using EasyWeb.AspNetCore.Swagger;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews()
-    .AddJsonOptions(options => { options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()); })
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+    })
     .AddEasyWebCore();
 
 builder.Services.ConfigureWebApiStandarts();
 
+builder.Services.ConfigureSwagger(options =>
+{
+    options.Title = "Demo Service Title";
+    options.Description = "Demo Service Description";
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+
+app.ApplySwaggerConfiguration();
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.MapControllers();
+
+app.MapDefaultControllerRoute();
 
 app.Run();
